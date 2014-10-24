6217b66ade1780deb3ab8a07a9649ea1c3b3a1c1
Long Long <longlong2010@gmail.com>
2014-10-24 22:13:40 +0800

    add gaussj2

---
+++ gaussj2.m
@@ -0,0 +1,36 @@
+function ydot = gaussj2(t, y)
+mu = 398601;
+j2 = 1.0826e-3;
+Re = 6378.14;
+ydot = zeros(6, 1);
+
+a = y(1);
+e = y(2);
+i = y(3);
+Omiga = y(4);
+omiga = y(5);
+M = y(6);
+
+T = 2 * pi / sqrt(mu) * a^(3 / 2);
+n = 2 * pi / T;
+psi = kepler(e, M);
+theta = 2 * atan(tan(psi / 2) * sqrt((1 + e) / (1 - e)));
+lr = (1 - e^2) * a *  (1 / (1 + e * cos(theta)));
+
+R = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * (1 - 3 * (sin(i))^2 * (sin(omiga + theta))^2);
+S = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * (sin(i))^2 * sin(2 * (omiga + theta));
+W = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * sin(2 * i) * sin(omiga + theta);
+
+%a
+ydot(1) = 2 / (n * sqrt(1 - e^2)) * (e * sin(theta) * R + (1 + e * cos(theta)) * S);
+%e
+ydot(2) = sqrt(1 - e^2) / (n * a) * (sin(theta) * R + (cos(psi) + cos(theta)) * S);
+%i
+ydot(3) = 1 / (n * a * sqrt(1 - e^2)) * (lr / a) * cos(theta + omiga) * W;
+%Omiga
+ydot(4) = 1 / (n * a * sqrt(1 - e^2)) * (lr / a) * (sin(theta + omiga) / sin(i)) * W;
+%omiga
+ydot(5) = sqrt(1 - e^2) / (n * a * e) * (-R * cos(theta) + (1 + 1 / (1 + e * cos(theta))) * sin(theta) * S) - ydot(4) * cos(i) * W;
+%M
+ydot(6) = n + (1 - e^2) / (n * a * e) * ((-2 * e / (1 + e * cos(theta)) + cos(theta)) * R - (1 + 1 / (1 + e * cos(theta)) * sin(theta) * S));
+end
--- main3.m
+++ main3.m
@@ -19,19 +19,32 @@ y0 = [r0; v0];
 
 T = 2 * pi * a^(3 / 2) / sqrt(mu);
 
-tspan=[0 4 * T];
+%tspan=[0 4 * T];
+%options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
+%[t, y] = ode45('eomj2', tspan, y0, options);
+%
+%steps = length(y);
+%orbit_elements = zeros(steps, 5);
+%
+%for k = 1:steps
+%	s = elements(y(k, 1:3), y(k, 4:6));
+%	orbit_elements(k, :) = s(1:5);
+%end
+%
+%for k = 1:5
+%	figure(k);
+%	plot(t, orbit_elements(:,k));
+%end
+
+
+M = 2 * atan(sqrt((1 - e)/(1 + e)) * tan(theta * pi / 360)) - e * sqrt(1 - e^2) * sin(theta * pi / 180) / (1 + e * cos(theta * pi / 180));
+y0 = [a; e; i * pi / 180; Omiga * pi / 180; omiga * pi / 180; M];
+
+tspan=[0 40];
 options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
-[t, y] = ode45('eomj2', tspan, y0, options);
-
-steps = length(y);
-o_elements = zeros(steps, 5);
-
-for k = 1:steps
-	s = elements(y(k, 1:3), y(k, 4:6));
-	o_elements(k, :) = s(1:5);
-end
+[t, y] = ode45('gaussj2', tspan, y0, options);
 
 for k = 1:5
 	figure(k);
-	plot(t, o_elements(:,k));
+	plot(t, y(:,k));
 end
