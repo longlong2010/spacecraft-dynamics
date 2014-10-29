b5718afec55c86dab71a8e3093e2e0f98f4dbc7d
Long Long <longlong2010@gmail.com>
2014-10-29 21:29:01 +0800

    remove comment

--- gaussj2.m
+++ gaussj2.m
@@ -1,5 +1,4 @@
 function ydot = gaussj2(t, y)
-%常数
 mu = 398601;
 j2 = 1.0826e-3;
 Re = 6378.14;
@@ -15,17 +14,14 @@ M = y(6);
 
 T = 2 * pi / sqrt(mu) * a^(3 / 2);
 n = 2 * pi / T;
-%求解开普勒方程
 psi = kepler(e, M);
 theta = 2 * atan(tan(psi / 2) * sqrt((1 + e) / (1 - e)));
 lr = (1 - e^2) * a *  (1 / (1 + e * cos(theta)));
 
-%摄动力
 R = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * (1 - 3 * (sin(i))^2 * (sin(omiga + theta))^2);
 S = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * (sin(i))^2 * sin(2 * (omiga + theta));
 W = -mu / lr^2 * (3 / 2) * j2 * (Re / lr)^2 * sin(2 * i) * sin(omiga + theta);
 
-%高斯方程
 
 %a
 ydot(1) = 2 / (n * sqrt(1 - e^2)) * (e * sin(theta) * R + (1 + e * cos(theta)) * S);
