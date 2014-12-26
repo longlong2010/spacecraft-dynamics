9bc201dc2c1fa41e439ca682b7442bc624aa8f57
Long Long <longlong2010@gmail.com>
2014-12-26 20:50:31 +0800

    add new function

--- main6.m
+++ main6.m
@@ -69,15 +69,7 @@ psi = -60 * pi / 180;
 
 omiga0 = sqrt(mu / (Re + ha)^3);
 
-A(1, 1) = cos(theta) * cos(psi);
-A(1, 2) = cos(theta) * sin(psi);
-A(1, 3) = -sin(theta);
-A(2, 1) = -cos(phi) * sin(psi) + sin(phi) * sin(theta) * cos(psi);
-A(2, 2) = cos(phi) * cos(psi) + sin(phi) * sin(theta) * sin(psi);
-A(2, 3) = sin(phi) * cos(theta);
-A(3, 1) = sin(phi) * sin(psi) + cos(phi) * sin(theta) * cos(psi);
-A(3, 2) = -sin(phi) * cos(psi) + cos(phi) * sin(theta) * sin(psi);
-A(3, 3) = cos(phi) * cos(theta);
+A = angle2cosine(phi, theta, psi);
 
 qt = cosine2q(A);
 
